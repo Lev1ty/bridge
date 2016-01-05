11b49770bc3b77d43e6c43a99e11a9370e42808d
VendorSniper <adamxhy123@outlook.com>
2016-01-04 20:15:34 -0500

    Unknown changes

--- src/main/Bridge.java
+++ src/main/Bridge.java
@@ -1,11 +1,15 @@
 package main;
 
+import java.util.*;
+
 /**
  * Created by Adam on 12/28/2015.
  */
 public class Bridge {
-    public static void main (String[]args)
-    {
-
+    public static void main (String[]args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a+b);
     }
 }
